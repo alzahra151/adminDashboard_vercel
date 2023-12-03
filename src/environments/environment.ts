035c9579f44d76d93137d3fdd69c4d649a72e6ab
Alzahra mohamed <alzahradesoky.15@gmail.com>
2023-12-03 23:16:05 +0200

    add req updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // apiUrl: "http://localhost:3000",
-    apiUrl: "https://varrox-system-apii.onrender.com"
+    apiUrl: "http://localhost:3000",
+    // apiUrl: "https://varrox-system-apii.onrender.com"
 
 
 };
